65721e454f8eea63dbd5f848abd6042a651b4a11
pyohei <pyohei@users.noreply.github.com>
2017-03-19 12:34:23 +0900

    細かい制御の修正
    ファイルを削除するのではなく、移動するように修正（案レベル）

--- bundle_files.cmd
+++ bundle_files.cmd
@@ -7,116 +7,142 @@ rem 処理フロー
 rem   - 各種設定
 rem   - ファイルのコピー
 rem   - ファイルの削除
-rem
+rem 
+rem オプション
+rem   /test/ ... テストモード。ファイルのコピーのみする。
+rem 
 rem その他
 rem   %USERPROFILE%\archive_ignores.ini に無視ファイルorディレクトリを記載
 rem   すると無視するような仕様とした
 
+echo Start. This log is in %USERPROFILE%\archives\log.txt
 
 rem 遅延環境変数の展開（for内の変数が上手く動作するように）
 rem 変数を使用するときも%ではなく!を使用する
-setlocal ENABLEDELAYEDEXPANSIONS
+setlocal ENABLEDELAYEDEXPANSION
 
-echo ---process start---
+call :logging ---process start---
 set start_date=%date%
 set start_time=%time%
-echo %date%%time%
+call :logging  %date%%time%
 
 rem 引数が指定されていない時はから文字列が入り、
 rem 文字列判定が正しく行われなくなるため、ダミー文字を付ける
 
 set arg1=%1/
-echo arg1: %arg1%
+call :logging arg1: %arg1%
 
 set format_date=%date:/=_%
 set format_time=%time::=%
 set format_time=%format_time: =%
-echo start_time: %format_date%%format_time%
+call :logging start_time: %format_date%%format_time%
 
 rem ====取得先フォルダ====
 set target_dir=%USERPROFILE%\Desktop
 rem ====保存先フォルダ=====
 set save_dir=%USERPROFILE%\Documents\archives
-echo save_dir: %save_dir%
+call :logging save_dir: %save_dir%
 rem ====ファイルを集めるフォルダ名====
 set dir_name=archive_%format_date%_%format_time%
-echo dir_name: %dir_name%
+call :logging dir_name: %dir_name%
 set save_dir=%save_dir%\%dir_name%
+call :logging exe_dir: %save_dir%
 
 rem ====実行ファイルのパス取得====
 set exe_dir=%~dp0
-echo exe_dir: %exe_dir%
+call :logging exe_dir: %exe_dir%
 
 mkdir %save_dir%
 cd %target_dir%
 
 rem ====ファイル・フォルダを一か所にコピーする====
 rem ディレクトリの時はファイル構造ごとコピーする
+
 for /f "usebackq delims=:" %%i in (`dir /b`) do (
-    set copy_source=%target_dir%\%%i
-    echo source: !copy_source!
-    echo dest: %save_dir%
+    rem set copy_source=%target_dir%\%%i
+    set file=%%i
+    echo !file!
+    call :logging source: !copy_source!
+    call :logging dest: %save_dir%
     
+    echo !copy_source!
+
     call :isTarget %%i
+
     if errorlevel 1 (
-        echo Ignore File
+        call :logging Ignore
     ) else (
         if exist !copy_source!\ (
             xcopy !copy_source! %save_dir%\%%i\ /e 
-            echo ---copy directory---
+            call :logging ---copy directory---
+            move %copy_source% %save_dir%\%%i
+            if errorlevel 1 (
+                call :logging Move Error
+            )
         ) else (
             copy !copy_source! %save_dir%\ 
-            echo ---copy file---
-        )
-    )
-)
-
-rem ====ファイル・フォルダを削除する====
-rem ディレクトリの時はファイル構造ごとコピーする
-rem テストモードではファイル・フォルダの削除はしない
-if not %arg1% == /test/ (
-    for /f "usebackq delims=:" %%i in (`dir /b`) do (
-        set copy_source=%target_dir%\%%i
-        
-        call :isTarget %%i
-        if errorlevel 1 (
-            echo Ignore File
-        ) else (
-            if exist !copy_source!\ (
-                rd /s /q !copy_source!
-                echo ---delete directory---
-            ) else (
-                del !copy_source!
-                echo ---delete file---
+            call :logging ---copy file---
+            move %copy_source% %save_dir%\
+            if errorlevel 1 (
+                call :logging Move Error
             )
         )
     )
 )
 
+
+rem rem ====ファイル・フォルダを削除する====
+rem rem ディレクトリの時はファイル構造ごとコピーする
+rem rem テストモードではファイル・フォルダの削除はしない
+rem if not %arg1% == /test/ (
+rem     for /f "usebackq delims=:" %%i in (`dir /b`) do (
+rem         set copy_source=%target_dir%\%%i
+rem         
+rem         call :isTarget %%i
+rem         if errorlevel 1 (
+rem             echo Ignore File
+rem         ) else (
+rem             if exist !copy_source!\ (
+rem                 rd /s /q !copy_source!
+rem                 echo ---delete directory---
+rem             ) else (
+rem                 del !copy_source!
+rem                 echo ---delete file---
+rem             )
+rem         )
+rem     )
+rem )
+
 cd %exe_dir%
 
-set end_date=%date%
-set end_time=%time%
-echo ---process end---
-echo start_time: %start_date% %start_time%
-echo end_time  : %end_date% %end_time%
+call :logging ---process end---
 
 endlocal
+echo Finish.
 
-rem ファイル/ディレクトリが対象かどうか判定する
-rem ERRORLEVELで判定できるようなサブルーチンとした
+rem ===========
+rem ファイル/ディレクトリが対象かどうか判定。対象可否は%ERRORLEVEL%で判定
+
 :isTarget
 set IGNORE_FILE=%USERPROFILE%\archive_ignores.ini
 if exist %IGNORE_FILE% (
-    echo =====
-    echo %1
     for /f "delims=" %%i in (%IGNORE_FILE%) do (
-        echo %%i
-        echo "%1" "%%i"
         if "%1" == "%%i" (
-            echo ignore yahoo
+            call :logging %1 is ignore.
             exit /B 1
         )
     )
 )
 exit /B 0
+
+rem ===========
+rem ログ作成。ファイルが存在しない場合は、ディレクトリとファイルを作成する。
+:logging
+set BACKUP_DIR=%USERPROFILE%\Documents\archives
+set LOG_NAME=%BACKUP_DIR%\log.txt
+
+if not exist %BACKUP_DIR% (
+    mkdir %BACKUP_DIR%
+)
+
+echo [%DATE% %TIME:~0,8%] %* >> %LOG_NAME%
